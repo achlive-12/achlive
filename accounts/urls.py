629e6663a1e60bcdc94fa222f7806d8c22dbaf8d
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2024-07-16 07:31:04 -0500

    New update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path,include
 from .views import *
-from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
+from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
+
 urlpatterns = [
     path('history/', DashboardView.as_view(), name="history"),
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('', include('dj_rest_auth.urls')),
-    path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password/reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password/reset/', reset_password_request_token, name='password_reset'),
+    path('passwordreset/confirm/', reset_password_confirm, name='password_reset_confirm'),
 ]
