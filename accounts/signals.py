e074ffa1f2b80182e42c0117042ccfb5f83f4a58
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2024-07-16 08:31:48 -0500

    new update

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,24 +1,24 @@
 from django.dispatch import receiver
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.urls import reverse
+
 
 @receiver(reset_password_token_created)
 def handle_password_reset_token(sender, instance, reset_password_token, *args, **kwargs):
-    user = instance.user  # Get the user associated with the token
-    uid = urlsafe_base64_encode(force_bytes(user.pk))  # Encode user ID
-    token = default_token_generator.make_token(user)  # Generate token
+     # Encode user ID
+    token = reset_password_token.key  # Generate token
 
     # Construct the reset link
-    reset_link = f"https://erblan.com/reset-password?token={token}&uid={uid}"
-
+    reset_link = f"https://erblan.com/reset-password?token={token}"
+    user = reset_password_token.user
     # Send the reset link to the user's email
     subject = "Password Reset Link"
     message = f"Click the link below to reset your password:\n\n{reset_link}"
     from_email = "support@erblan.com"  # Set your sender email address
     recipient_list = [user.email]
-
-    send_mail(subject, message, from_email, recipient_list)
+    try:
+        send_mail(subject, message, from_email, recipient_list)
+        print(f"Password reset email sent to {user.email}")
+    except Exception as e:
+        print(e)
+        pass
