ee71bbdc7e843eee0c947251fbd9bc36f2a0a628
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2024-09-18 17:31:09 -0500

    Update views.py

--- store/views.py
+++ store/views.py
@@ -2,15 +2,31 @@ from .serializers import *
 from .models import *
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
+#import pagination
+from rest_framework.pagination import PageNumberPagination
+
 
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all().exclude(name="Extraction")
     serializer_class = CategorySerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    pagination_class = PageNumberPagination
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+
+
 
 class CategoryListView(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         category_slug = self.kwargs['category_slug']
@@ -20,8 +36,10 @@ class CategoryListView(generics.ListAPIView):
     
     def get(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        serializer = ProductSerializer(queryset, many=True)
-        if queryset:
-            return Response(serializer.data)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
         else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
