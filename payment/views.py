30aa7656cab3d96498ba03ca17075b891ecf6bd5
DeAgusco <149245943+achlive-12@users.noreply.github.com>
2024-09-29 20:06:44 +0000

    Update views.py

--- payment/views.py
+++ payment/views.py
@@ -52,7 +52,7 @@ class CoinbasePaymentView(APIView):
                 # If address does not exist, generate a new address
                 api_key = 'pcXP1sIXrsBqYcOyv9dzofmaWCq4bo6aJL9LDadYhEc'
                 amount = float(1.00)
-                url = 'https://www.blockonomics.co/api/new_address'
+                url = 'https://www.blockonomics.co/api/new_address?match_account=BzwyYp'
                 headers = {'Authorization': "Bearer " + api_key}
 
                 # Call Blockonomics API to generate a new address
